6ed8d73989fa342a39860978877b484c8330f8b4
froorider@gmail.com <froorider@gmail.com@f67990d4-2cfa-11de-9cad-a3574e5575ef>
2009-04-19 16:08:32 +0000

    Initial check-in

---
+++ trunk/TomsCodeHeap/TomsCodeHeap/Persistence/PersistenceManager.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+using System.IO;
+using ch.froorider.codeheap.Domain;
+
+namespace ch.froorider.codeheap.Persistence
+{
+    /// <summary>
+    /// The persistence manager class offers two generic methods to serialize and deserialize objects. The serialization method is
+    /// implemented as an extension method. So it can be used / called directly on any object.
+    /// </summary>
+    public static class PersistenceManager
+    {
+        #region fields
+
+        private static readonly string fileNameExtension = ".GDC";
+        private static readonly string filePath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\NGoogleCalendar\\";
+
+        #endregion
+
+        #region constructors
+
+        /// <summary>
+        /// Constructor. Inits some things like the directories etc.
+        /// </summary>
+        static PersistenceManager()
+        {
+            lock (filePath)
+            {
+                if (!Directory.Exists(filePath))
+                {
+                    Directory.CreateDirectory(filePath);
+                }
+            }
+        }
+
+        #endregion
+
+        #region public methods (functionality)
+
+        /// <summary>
+        /// Serializes the specified object to an XML - File.
+        /// The mthod returns as result the filename (without extension) where the contents of the object has been saved.
+        /// </summary>
+        /// <param name="objectToSerialize">The object to serialize. Must implement Serializable.</param>
+        /// <returns>The content of the object as an MD5 Hash string.</returns>
+        /// <exception cref="System.InvalidOperationException">Is thrown when the object is not suited for serialization.</exception>
+        public static string Serialize(this BusinessObject objectToSerialize)
+        {
+            if (objectToSerialize.GetType().IsSerializable)
+            {
+                string filename = MD5HashGenerator.GenerateKey(objectToSerialize);
+                XmlSerializer serializer = new XmlSerializer(objectToSerialize.GetType());
+                using (TextWriter textWriter = new StreamWriter(filePath + filename + fileNameExtension))
+                {
+                    serializer.Serialize(textWriter, objectToSerialize);
+                    textWriter.Close();
+                }
+                objectToSerialize.ObjectIdentifier = filename;
+                return filename;
+            }
+            else
+            {
+                throw new InvalidOperationException("Only types which are marked as Serializable are supported.");
+            }
+        }
+
+        /// <summary>
+        /// Deserializes an object.
+        /// </summary>
+        /// <typeparam name="T">Type of the object which you expect.</typeparam>
+        /// <param name="filename">The filename (without extension) of the file which contains the persistent data.</param>
+        /// <returns>An object of desired type when the file could be deserialized.</returns>
+        public static T DeserializeObject<T>(string filename)
+        {
+            if (String.IsNullOrEmpty(filename))
+            {
+                throw new ArgumentException("Filename cannot be null or empty");
+            }
+
+            T deserializedObject;
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+            using (TextReader textReader = new StreamReader(filePath + filename + fileNameExtension))
+            {
+                deserializedObject = (T)serializer.Deserialize(textReader);
+                textReader.Close();
+            }
+            return deserializedObject;
+        }
+
+        #endregion
+    }
+}
---
+++ trunk/TomsCodeHeap/TomsCodeHeap/Testing/AsserException.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ch.froorider.codeheap.Testing
+{
+    /// <summary>
+    /// Contains assertion types for exceptions that are not provided with the standard MSTest assertions.
+    /// </summary>
+    /// <remarks>
+    /// The standard test framework has an Attribute called <see cref="ExpectedExceptionAttribute">ExpectedExceptionAttribute</see>. This attribute has two
+    /// main disadvantages:
+    /// <para>
+    /// 1. The unit test stops at the line which throws the expected exception. If you want to test a method which throws a bunch of exceptions
+    /// you must write a test for each exception.
+    /// 2. The attribute does not specify exactly where the exception has to be thrown. So if a method call earlier than expected throws
+    /// suddenly the same exception, the whole test is still o.k.
+    /// </para>
+    /// So this class can be used like the common assertions. You can test a method at a sepcific line in the test for a specific exception.
+    /// </remarks>
+    public static class AssertException
+    {
+        #region asserts for methods with signature: void MethodName(parameter, ...)
+
+        /// <summary>
+        /// Checks to make sure that the input delegate throws a exception of type TException.
+        /// <para>
+        /// The input delegate must be a method with no parameters and return type void!
+        /// </para>
+        /// </summary>
+        /// <typeparam name="TException">The type of exception expected.</typeparam>
+        /// <param name="methodToExecute">The method to execute.</param>
+        public static void Throws<TException>(Action methodToExecute) where TException : System.Exception
+        {
+            try
+            {
+                methodToExecute();
+            }
+            catch (Exception e)
+            {
+                Assert.IsTrue(e.GetType() == typeof(TException), "Expected exception of type " + typeof(TException) + " but type of " + e.GetType() + " was thrown instead.");
+                return;
+            }
+            Assert.Fail("Expected exception of type " + typeof(TException) + " but no exception was thrown.");
+        }
+
+        /// <summary>
+        /// Checks to make sure that the input delegate throws a exception of type TException with a specific exception message.
+        /// <para>
+        /// The input delegate must be a method with no parameters and return type void!
+        /// </para>
+        /// </summary>
+        /// <typeparam name="TException">The type of exception expected.</typeparam>
+        /// <param name="expectedMessage">The expected exception message.</param>
+        /// <param name="methodToExecute">The method to execute.</param>
+        /// <remarks>
+        /// This method asserts if the given message and the message of the thrown exception are not equal!
+        /// </remarks>
+        public static void Throws<TException>(string expectedMessage, Action methodToExecute) where TException : System.Exception
+        {
+            try
+            {
+                methodToExecute();
+            }
+            catch (Exception e)
+            {
+                Assert.IsTrue(e.GetType() == typeof(TException), "Expected exception of type " + typeof(TException) + " but type of " + e.GetType() + " was thrown instead.");
+                Assert.AreEqual(expectedMessage, e.Message, "Expected exception with a message of '" + expectedMessage + "' but exception with message of '" + e.Message + "' was thrown instead.");
+                return;
+            }
+            Assert.Fail("Expected exception of type " + typeof(TException) + " but no exception was thrown.");
+        }
+
+        /// <summary>
+        /// Checks to make sure that the input delegate throws a exception of type TException with a specific exception message.
+        /// <para>
+        /// The input delegate must be a method with no parameters and return type void!
+        /// </para>
+        /// </summary>
+        /// <typeparam name="TException">The type of exception expected.</typeparam>
+        /// <typeparam name="T">The type of the argument, that is passed to the method.</typeparam>
+        /// <param name="methodToExecute">The method to execute.</param>
+        /// <param name="argument">The argument.</param>
+        /// <remarks>
+        /// This method asserts if the given message and the message of the thrown exception are not equal!
+        /// </remarks>
+        public static void Throws<TException, T>(Action<T> methodToExecute, T argument) where TException : System.Exception
+        {
+            try
+            {
+                methodToExecute(argument);
+            }
+            catch (Exception e)
+            {
+                Assert.IsTrue(e.GetType() == typeof(TException), "Expected exception of type " + typeof(TException) + " but type of " + e.GetType() + " was thrown instead.");
+                return;
+            }
+            Assert.Fail("Expected exception of type " + typeof(TException) + " but no exception was thrown.");
+        }
+
+
+        #endregion
+
+        #region asserts for methods with signature: returnValue MethodName(parameter, ....)
+
+        /// <summary>
+        /// Checks to make sure that the input delegate throws a exception of type TException with a specific exception message.
+        /// <para>
+        /// The input delegate must be a method with ONE parameter and return type!
+        /// </para>
+        /// </summary>
+        /// <typeparam name="TException">The type of the exception.</typeparam>
+        /// <typeparam name="T">The type of the input argument</typeparam>
+        /// <typeparam name="TResult">The type of the result.</typeparam>
+        /// <param name="methodToExecute">The method to execute.</param>
+        /// <param name="argument">The argument to input.</param>
+        public static void Throws<TException, T, TResult>(Func<T, TResult> methodToExecute, T argument)
+            where TException : System.Exception
+        {
+            try
+            {
+                methodToExecute(argument);
+            }
+            catch (Exception e)
+            {
+                Assert.IsTrue(e.GetType() == typeof(TException), "Expected exception of type " + typeof(TException) + " but type of " + e.GetType() + " was thrown instead.");
+                return;
+            }
+            Assert.Fail("Expected exception of type " + typeof(TException) + " but no exception was thrown.");
+        }
+
+        #endregion
+
+        #region asserts for constructors
+
+        /// <summary>
+        /// Checks the default constructor.
+        /// </summary>
+        /// <typeparam name="TException">The type of the exception.</typeparam>
+        /// <param name="typeToCreate">The type to create.</param>
+        public static void Throws<TException>(Type typeToCreate)
+            where TException : System.Exception
+        {
+            try
+            {
+                Activator.CreateInstance(typeToCreate);
+            }
+            catch (Exception e)
+            {
+                Assert.IsTrue(e.InnerException.GetType() == typeof(TException), "Expected exception of type " + typeof(TException) + " but type of " + e.InnerException.GetType() + " was thrown instead.");
+                return;
+            }
+            Assert.Fail("Expected exception of type " + typeof(TException) + " but no exception was thrown.");
+        }
+
+        /// <summary>
+        /// Checks the constructor which takes one argument.
+        /// </summary>
+        /// <typeparam name="TException">The type of the exception to check</typeparam>
+        /// <typeparam name="T">The type of the argument.</typeparam>
+        /// <param name="typeToCreate">The type to create.</param>
+        /// <param name="argument">The argument itself.</param>
+        public static void Throws<TException, T>(Type typeToCreate, T argument)
+            where TException : System.Exception
+        {
+            try
+            {
+                object[] arguments = new object[1];
+                arguments[0] = argument;
+                Activator.CreateInstance(typeToCreate, arguments);
+            }
+            catch (Exception e)
+            {
+                Assert.IsTrue(e.InnerException.GetType() == typeof(TException), "Expected exception of type " + typeof(TException) + " but type of " + e.InnerException.GetType() + " was thrown instead.");
+                return;
+            }
+            Assert.Fail("Expected exception of type " + typeof(TException) + " but no exception was thrown.");
+        }
+
+        #endregion
+    }
+}
