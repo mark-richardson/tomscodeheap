cc308395f3e70e843a98ffe0132813d52f400ad3
froorider <froorider@gmail.com>
2009-05-15 14:59:53 +0000

    Added Interfaces for Scheduling.

---
+++ TomsCodeHeap/TomsCodeHeap/Threading/ISchedule.cs
@@ -0,0 +1,88 @@
+﻿//========================================================================
+//File:     ISchedule.cs
+//
+//Author:   $Author$
+//Date:     $LastChangeDate$
+//Revision: $Revision$
+//========================================================================
+//Copyright [2009] [$Author$]
+//
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+//
+//http://www.apache.org/licenses/LICENSE-2.0
+//
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+//========================================================================
+          
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ch.froorider.codeheap.Threading
+{
+    /// <summary>
+    /// This interface declares the offered functionality by a sheduled object.
+    /// A scheduled object can be managed by a Scheduler. The client using this
+    /// schedule defines when to be triggered.
+    /// As trigger acts an AutoResetEvent. This signal can be used by the client to
+    /// wake up.
+    /// </summary>
+    public interface ISchedule : IDisposable
+    {
+        #region Properties
+
+        /// <summary>
+        /// Gets the <see cref="Scheduler"/> that this <see cref="ISchedule"/> belongs to.
+        /// </summary>
+        Scheduler Owner
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Gets the period of this <see cref="ISchedule"/>.
+        /// </summary>
+        TimeSpan Period
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Gets the signal that a client of this <see cref="ISchedule"/>
+        /// waits for to be signaled periodically.
+        /// </summary>
+        AutoResetEvent ScheduleEvent
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this <see cref="ISchedule"/> is currently active.
+        /// </summary>
+        bool Enabled
+        {
+            get;
+
+            set;
+        }
+
+        /// <summary>
+        /// Gets the next time when <see cref="ScheduleEvent"/> will be <see cref="AutoResetEvent.Set"/>;
+        /// or <see cref="DateTime.MaxValue"/> if <see cref="Enabled"/> is <see langword="false"/>.
+        /// </summary>
+        DateTime NextDueTime
+        {
+            get;
+        }
+
+        #endregion Properties
+    }
+}
