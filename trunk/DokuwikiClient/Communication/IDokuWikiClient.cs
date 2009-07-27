ec1290cc103d7f77eaec654f0a5a3afa4340d972
froorider@gmail.com <froorider@gmail.com@f67990d4-2cfa-11de-9cad-a3574e5575ef>
2009-07-27 15:51:40 +0000

    First Check-in of project

---
+++ trunk/DokuwikiClient/Communication/IDokuWikiClient.cs
@@ -0,0 +1,68 @@
+﻿// ========================================================================
+// File:     IDokuWikiClient.cs
+//
+// Author:   $Author$
+// Date:     $LastChangedDate$
+// Revision: $Revision$
+// ========================================================================
+// Copyright [2009] [$Author$]
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ========================================================================
+
+using CookComputing.XmlRpc;
+
+namespace DokuwikiClient.Communication
+{
+	/// <summary>
+	/// Definition of the remote procedure calls of the DokuWikiServer. 
+	/// </summary>
+	public interface IDokuWikiClient : IXmlRpcProxy
+	{
+		#region DokuWiki specific remote methods
+
+		/// <summary>
+		/// Lists all pages within a given namespace.
+		/// </summary>
+		/// <param name="nameSpace">The namespace which should be searched.</param>
+		/// <param name="options">The options for the php method searchAllPages().</param>
+		/// <returns>
+		/// A string of the page item names in this namespace.
+		/// </returns>
+		/// <remarks>The options are passed directly to the PHP method searchAllPages().</remarks>
+		[XmlRpcMethod("dokuwiki.getPagelist")]
+		string[] GetPageList(string nameSpace, string[] options);
+
+		/// <summary>
+		/// Gets the doku wiki version.
+		/// </summary>
+		/// <returns>A string containing the version number of the remote dokuwiki.</returns>
+		[XmlRpcMethod("dokuwiki.getVersion")]
+		string GetDokuWikiVersion();
+
+		/// <summary>
+		/// Gets the current time at the remote wiki server as Unix timestamp. 
+		/// </summary>
+		/// <returns>An integer value indicating the server time.</returns>
+		[XmlRpcMethod("dokuwiki.getTime")]
+		int GetTime();
+
+		#endregion
+
+		#region common remote methods
+
+		// [XmlRpcMethod("")]
+		// string MethodName(string parameter);
+		#endregion
+	}
+}
---
+++ trunk/DokuwikiClient/Communication/XmlRpcClient.cs
@@ -0,0 +1,133 @@
+﻿// ========================================================================
+// File:     XmlRpcClient.cs
+//
+// Author:   $Author$
+// Date:     $LastChangeDate$
+// Revision: $Revision$
+// ========================================================================
+// Copyright [2009] [$Author$]
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ========================================================================
+
+using System;
+using System.IO;
+using System.Net;
+using CookComputing.XmlRpc;
+using log4net;
+
+namespace DokuwikiClient.Communication
+{
+	/// <summary>
+	/// Proxy class for the communication between the program and the XmlRpcServer.
+	/// Wraps all the remote method calls in a common way.
+	/// </summary>
+	internal class XmlRpcClient
+	{
+		#region fields
+
+		private static ILog logger = LogManager.GetLogger(typeof(XmlRpcClient));
+
+		private IDokuWikiClient clientProxy;
+
+		#endregion
+
+		#region Constructor
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="XmlRpcClient"/> class.
+		/// </summary>
+		/// <param name="serverUrl">The server URL.</param>
+		/// <exception cref="ArgumentException">Is thrown when the passed server URL was not valid.</exception>
+		public XmlRpcClient(Uri serverUrl)
+		{
+			try
+			{
+				this.clientProxy = XmlRpcProxyGen.Create<IDokuWikiClient>();
+				this.clientProxy.NonStandard = XmlRpcNonStandard.AllowNonStandardDateTime;
+				this.clientProxy.Url = serverUrl.AbsoluteUri;
+
+#if(TRACE)
+				RequestResponseLogger dumper = new RequestResponseLogger();
+				dumper.Directory = @"C:\logs\xmlrpctests";
+				if (!Directory.Exists(dumper.Directory))
+				{
+					Directory.CreateDirectory(dumper.Directory);
+				}
+
+				dumper.Attach(this.clientProxy);
+#endif
+
+				Console.WriteLine("XmlRpc proxy to URL: " + serverUrl.AbsoluteUri + " generated.");
+			}
+			catch (UriFormatException ufe)
+			{
+				Console.WriteLine(ufe);
+				throw new ArgumentException("serverUrl", "Server URL is not valid. Cause: " + ufe.Message);
+			}
+		}
+
+		#endregion
+
+		#region Introspection API
+
+		/// <summary>
+		/// Returns a list of methods implemented by the server.
+		/// </summary>
+		/// <returns>An array of strings listing all remote method names.</returns>
+		public string[] ListServerMethods()
+		{
+			try
+			{
+				return this.clientProxy.SystemListMethods();
+			}
+			catch (WebException we)
+			{
+				logger.Warn(we);
+				string[] errorMessage = { "URL to remote server is not valid." };
+				return errorMessage;
+			}
+		}
+
+		/// <summary>
+		/// Gives you a list of possible methods implemented at the server.
+		/// </summary>
+		/// <param name="methodName">Name of the method.</param>
+		/// <returns>An array containing all method signatures this remote method call offers.</returns>
+		public object[] GetMethodSignatures(string methodName)
+		{
+			try
+			{
+				return this.clientProxy.SystemMethodSignature(methodName);
+			}
+			catch (XmlRpcFaultException xrfe)
+			{
+				logger.Warn(xrfe);
+				string[] errorMessage = { "Unknown method name" };
+				return errorMessage;
+			}
+		}
+
+		/// <summary>
+		/// Gives you a string describing the use of a certain method.
+		/// </summary>
+		/// <param name="methodName">Name of the method.</param>
+		/// <returns>A description for the usage of this remote method.</returns>
+		public string GetMethodHelp(string methodName)
+		{
+			return this.clientProxy.SystemMethodHelp(methodName);
+		}
+
+		#endregion
+	}
+}
