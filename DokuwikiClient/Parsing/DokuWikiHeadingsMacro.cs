9201e945c0bda768083f5c10a51786c3d75b50ae
froorider <froorider@gmail.com>
2009-09-01 17:32:10 +0000

    Added a first macro, renderer etc. for formatting H1 - Headers with WikiPlex.

---
+++ DokuwikiClient/Parsing/DokuWikiHeadingsMacro.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WikiPlex.Compilation.Macros;
+using WikiPlex.Compilation;
+using WikiPlex;
+
+namespace DokuwikiClient.Parsing
+{
+	/// <summary>
+	/// Lexer to find the Headings in wikitext.
+	/// </summary>
+	public class DokuWikiHeadingsMacro : IMacro
+	{
+		#region IMacro Members
+
+		public string Id
+		{
+			get { return "DokuWikiHeadingsMacro"; }
+		}
+
+		public IList<WikiPlex.Compilation.MacroRule> Rules
+		{
+			get 
+			{
+				return new List<MacroRule>
+                           {
+                               new MacroRule(EscapeRegexPatterns.CurlyBraceEscape),
+                               new MacroRule(@"((?<=)={6})(.*)((?=)={6})",
+                                             new Dictionary<int, string>
+                                                 {
+                                                     {1, ScopeName.Remove},
+                                                     {2, DokuWikiScope.HeadingOne},
+                                                     {3, ScopeName.Remove}
+                                                 }
+                                   )
+                           };
+			}
+		}
+
+		#endregion
+	}
+}
---
+++ DokuwikiClient/Parsing/DokuWikiScope.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DokuwikiClient.Parsing
+{
+	public class DokuWikiScope
+	{
+		public const string HeadingOne = "Heading One";
+	}
+}
