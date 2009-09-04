d9ae75027777b799c8a1851c87edcc227a6199cc
froorider <froorider@gmail.com>
2009-09-04 15:59:36 +0000

    Added the persistence to save wikipages locally Added a TestProject Added a Domain, with Entities

---
+++ DokuWikiClientTests/FileManagerTest.cs
@@ -0,0 +1,79 @@
+﻿using DokuwikiClient.Persistence;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CH.Froorider.Codeheap.Domain;
+using CH.Froorider.Codeheap.Persistence;
+using DokuwikiClient.Domain.Entities;
+using System;
+
+namespace DokuWikiClientTests
+{
+    /// <summary>
+    ///This is a test class for FileManagerTest and is intended
+    ///to contain all FileManagerTest Unit Tests
+    ///</summary>
+	[TestClass()]
+	public class FileManagerTest
+	{
+		private TestContext testContextInstance;
+
+		/// <summary>
+		///Gets or sets the test context which provides
+		///information about and functionality for the current test run.
+		///</summary>
+		public TestContext TestContext
+		{
+			get
+			{
+				return testContextInstance;
+			}
+			set
+			{
+				testContextInstance = value;
+			}
+		}
+
+		#region Additional test attributes
+		// 
+		//You can use the following additional attributes as you write your tests:
+		//
+		//Use ClassInitialize to run code before running the first test in the class
+		//[ClassInitialize()]
+		//public static void MyClassInitialize(TestContext testContext)
+		//{
+		//}
+		//
+		//Use ClassCleanup to run code after all tests in a class have run
+		//[ClassCleanup()]
+		//public static void MyClassCleanup()
+		//{
+		//}
+		//
+		//Use TestInitialize to run code before running each test
+		//[TestInitialize()]
+		//public void MyTestInitialize()
+		//{
+		//}
+		//
+		//Use TestCleanup to run code after each test has run
+		//[TestCleanup()]
+		//public void MyTestCleanup()
+		//{
+		//}
+		//
+		#endregion
+
+
+		[TestMethod()]
+		public void RegisterTest()
+		{
+			FileManager manager = new FileManager();
+			WikiAccount account = new WikiAccount();
+			account.WikiUrl = new Uri("http://wiki.froorider.ch/lib/exe/xmlrpc.php");
+			account.AccountName = "Froorider's wiki";
+			account.LoginName = "foobar";
+			account.Password = "barfoo";
+			account.Serialize();
+			manager.Register<WikiAccount>(account);
+		}
+	}
+}
---
+++ DokuwikiClient/Domain/Entities/WikiAccount.cs
@@ -0,0 +1,65 @@
+﻿// ========================================================================
+// File:     WikiAccount.cs
+// 
+// Author:   $Author$
+// Date:     $LastChangedDate$
+// Revision: $Revision$
+// ========================================================================
+// Copyright [2009] [$Author$]
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ========================================================================
+          
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CH.Froorider.Codeheap.Domain;
+
+namespace DokuwikiClient.Domain.Entities
+{
+	/// <summary>
+	/// A wiki acccount contains information about the remote wiki, like Accountname, Uri, login etc.
+	/// </summary>
+	[Serializable]
+	public class WikiAccount : BusinessObject
+	{
+		#region Properites
+
+		/// <summary>
+		/// Gets or sets the name of the account.
+		/// </summary>
+		/// <value>The name of the account.</value>
+		public string AccountName { get; set; }
+		
+		/// <summary>
+		/// Gets or sets the name of the login.
+		/// </summary>
+		/// <value>The name of the login.</value>
+		public string LoginName { get; set; }
+		
+		/// <summary>
+		/// Gets or sets the password.
+		/// </summary>
+		/// <value>The password.</value>
+		public string Password { get; set; }
+		
+		/// <summary>
+		/// Gets or sets the wiki URL.
+		/// </summary>
+		/// <value>The wiki URL.</value>
+		public Uri WikiUrl { get; set; }
+
+		#endregion
+	}
+}
