1b58fecda9294da4d1e22841f7d64d83433d8208
froorider <froorider@gmail.com>
2012-02-27 15:50:22 +0000

    Extended to use ImportMany

---
+++ TomsCodeHeap/JamesSharpContracts/Protocols/IProtocolData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CH.Froorider.JamesSharpContracts.Protocols
+{
+    public interface IProtocolData
+    {
+        int PortNumber { get; }
+        string ProtocolName { get; }
+    }
+}
--- TomsCodeHeap/JamesSharpPop/PopMock.cs
+++ TomsCodeHeap/JamesSharpPop/PopMock.cs
@@ -1,29 +1,20 @@
 ﻿using CH.Froorider.JamesSharpContracts.Protocols;
 using System.ComponentModel.Composition;
-using System;
-using JamesSharpSmtp.SmtpProtocol;
 using System.Text;
+using System;
 
-namespace JamesSharpSmtp
+namespace JamesSharpPop
 {
     [Export(typeof(IProtocol))]
-    [ExportMetadata("PortNumber", 25)]
-    [ExportMetadata("ProtocolName", "SMTP")]
-    public class SmtpMock : BaseProtocol
+    [ExportMetadata("PortNumber", 335)]
+    [ExportMetadata("ProtocolName", "POP3")]
+    public class PopMock : BaseProtocol
     {
         private byte[] _buffer = new byte[1024];
 
-        public override ProtocolType TypeOfProtocol
-        {
-            get
-            {
-                return SmtpProtocolType.SmtpProtocol;
-            }
-        }
-
         public override void ProcessConnection(object message)
         {
-            Console.WriteLine("James Sharp SMTP Server is processing message.");
+            Console.WriteLine("James Sharp POP Server is processing message.");
             StringBuilder completeMessage = new StringBuilder();
             // Incoming message may be larger than the buffer size.
             do
@@ -35,7 +26,7 @@ namespace JamesSharpSmtp
 
             Console.WriteLine("Received message: " + completeMessage);
 
-            _buffer = Encoding.ASCII.GetBytes("HELO you!");
+            _buffer = Encoding.ASCII.GetBytes("POP3 implemented!");
             StreamToProcess.Write(_buffer, 0, _buffer.Length);
             StreamToProcess.Close();
         }
